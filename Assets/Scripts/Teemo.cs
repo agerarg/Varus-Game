844743042ace429e1d5009eadd5fbcff96771d45
agerarg <ager.arg@gmail.com>
2019-06-03 19:44:13 -0300

    new

---
+++ Assets/Scripts/Teemo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("golpeo");
+        if (collision.gameObject.tag == "VarusShot")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/VarusMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarusMove : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input.GetAxis("Vertical")
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+        transform.position += Movement * speed * Time.deltaTime;
+    }
+}
